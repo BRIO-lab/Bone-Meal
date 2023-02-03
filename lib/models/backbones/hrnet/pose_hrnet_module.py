74313bde160d42e9517450e22ea12e382527cd94
CWDE615 <cwellis001@gmail.com>
2023-02-03 11:52:15 -0500

    Add TODO annotation to pose_hrnet_module

--- lib/models/backbones/hrnet/pose_hrnet_module.py
+++ lib/models/backbones/hrnet/pose_hrnet_module.py
@@ -23,6 +23,7 @@ class SegmentationNetModule(pl.LightningModule):
         print("Pose HRNet is on device " + str(next(self.pose_hrnet.parameters()).get_device()))     # testing line
         print("Is Pose HRNet on GPU? " + str(next(self.pose_hrnet.parameters()).is_cuda))            # testing line
         self.wandb_run = wandb_run
+        # CWDE: TODO: remove hard coding and replace with a parameter or query to config.net['LOSS'] processed by loss_selector or loss_manager
         self.loss_fn = torch.nn.BCEWithLogitsLoss()
         #print(self.pose_hrnet.get_device())
 
